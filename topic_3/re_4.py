59711e96169e96860b32d904ac5235631c755b61
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-04-11 15:11:16 +0300

    dz4

---
+++ topic_3/re_4.py
@@ -0,0 +1,11 @@
+import re
+
+text = "Вивчення Python може бути веселим."
+pattern = "Python"
+match = re.search(pattern, text)
+
+if match:
+    print("Знайдено:", match.group())
+else:
+    print("Не знайдено.")
+
